af74f6bacd17735c3bd05b0c210941c15640fbbe
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-11-09 18:50:37 +0300

    Done - add rating on movie api

--- server-side/addRatingOnMovie.php
+++ server-side/addRatingOnMovie.php
@@ -8,9 +8,23 @@ include "connection.php";
 
 
 $user_id = $_POST["user_id"];
-$movie_id = $_POST["movie_id"]
+$movie_id = $_POST["movie_id"];
 $rating_scale = $_POST["rating_scale"];
 
 
+$query = $connection->prepare("INSERT INTO ratings (user_id,movie_id,rating_scale) VALUES (?,?,?) ");
+$query->bind_param("iii", $user_id, $movie_id, $rating_scale);
+$query-> execute();
 
-$query = $connection->prepare("INSERT INTO  ")
+$result = $query->affected_rows;
+
+if($result != 0){
+    echo json_encode([
+        "status" => "Rating added",
+        "message" => "$result added to the ratings table"
+    ]);
+}else{
+    echo json_encode([
+        "status" => "Failure"
+    ]);
+}
