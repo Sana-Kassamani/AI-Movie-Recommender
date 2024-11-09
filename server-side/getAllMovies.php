08ae9df9e7ad2d64f15a3aecae06d448c9164b97
LyneSultan <lynesultane@gmail.com>
2024-11-09 17:41:30 +0200

    Get - all movies

---
+++ server-side/getAllMovies.php
@@ -0,0 +1,27 @@
+<?php
+
+include "connection.php";
+
+$query=$connection->prepare("SELECT title,img from movies");
+$query->execute();
+
+$result=$query->get_result();
+
+if($result->num_rows>0){
+
+  $movies=[];
+  while($movie=$result->fetch_assoc())
+  {
+    $movies[]=$movie;
+
+  }
+  echo json_encode($movies);
+} else {
+
+  $response = [
+    "message" => "Empty result"
+  ];
+
+  echo json_encode($response);
+}
+?>
