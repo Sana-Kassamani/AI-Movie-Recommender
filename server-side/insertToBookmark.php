b45c7b868bde4f99fe8c1048830533880ed58b8b
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-11-09 19:07:13 +0300

    Done - insert to bookmark api

--- server-side/insertToBookmark.php
+++ server-side/insertToBookmark.php
@@ -1,1 +1,27 @@
 <?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+include "connection.php";
+
+$user_id = $_POST["user_id"];
+$movie_id = $_POST["movie_id"];
+
+$query = $connection->prepare("INSERT INTO bookmarks(user_id, movie_id) VALUES (?,?)");
+$query->bind_param("ii", $user_id,$movie_id);
+$query->execute();
+
+$result = $query->affected_rows;
+
+if($result != 0){
+    echo json_encode([
+        "status" => "Bookmarked successfully",
+        "message" => "$result added to bookmarks table"
+    ]);
+}else{
+    echo json_encode([
+        "status" => "Failed to bookmark"
+    ]);
+}
