14d9433d207a969eb078d8126404f0bbfce0c428
LyneSultan <lynesultane@gmail.com>
2024-11-09 18:38:00 +0200

    Edit - movie details

--- server-side/getMovieDetails.php
+++ server-side/getMovieDetails.php
@@ -3,22 +3,23 @@ include "connection.php";
 
 $movie_id=$_POST['movie_id'];
 
-$query=$connection->prepare("SELECT * FROM movies WHERE ?");
+$query=$connection->prepare("SELECT * FROM movies WHERE movie_id=?");
 $query->bind_param("i",$movie_id);
 
 $query->execute();
-
 $result = $query->get_result();
-$movie = $result->fetch_assoc();
 
-if ($movie) {
+if($result->num_rows>0){
+  $movie = $result->fetch_assoc();
   echo json_encode([
       "message" => "Get movie details successful",
-      "movie" => $movie
+      "response" => $movie
   ]);
-} else {
+
+}else {
   echo json_encode([
       "message" => "Movie not found"
   ]);
 }
+
 ?>
