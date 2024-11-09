2063b61f742af32a5e83423bc3b4f11961d3751e
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-11-10 00:21:29 +0300

    Update - done with the insert/update activity

--- server-side/updateActivity.php
+++ server-side/updateActivity.php
@@ -11,8 +11,8 @@ $movie_id = $_POST["movie_id"];
 $nb_of_clicks = $_POST["nb_of_clicks"];
 $time_spent = $_POST["time_spent"];
 
-$query = $connection->prepare("UPDATE user_activities SET nb_of_clicks = ?, time_spent=? WHERE user_id = ? AND movie_id=?");
-$query->bind_param("iiii",$nb_of_clicks,$time_spent, $user_id,$movie_id,);
+$query = $connection->prepare("INSERT INTO user_activities (user_id, movie_id, nb_of_clicks, time_spent) VALUES (?,?,?,?) ON DUPLICATE KEY UPDATE nb_of_clicks = ?, time_spent=?");
+$query->bind_param("iiidid", $user_id,$movie_id, $nb_of_clicks,$time_spent, $nb_of_clicks,$time_spent);
 $query->execute();
 
 $result = $query->affected_rows;
@@ -20,7 +20,6 @@ $result = $query->affected_rows;
 if($result != 0){
     echo json_encode([
         "status" => "Updated successfully",
-        "message" => "$result added to activities table"
     ]);
 }else{
     echo json_encode([
