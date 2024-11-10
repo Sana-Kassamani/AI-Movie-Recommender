3ac55c5b04e1ebf1758359fac184884380228e8f
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-11-10 17:31:58 +0300

    Update - small modificiations to APIs

--- server-side/updateActivity.php
+++ server-side/updateActivity.php
@@ -10,21 +10,6 @@ $movie_id = $_POST["movie_id"];
 $nb_of_clicks = $_POST["nb_of_clicks"];
 $time_spent = $_POST["time_spent"];
 
-$select_query = $connection->prepare("SELECT nb_of_clicks, time_spent FROM user_activities WHERE user_id = ? AND movie_id = ?");
-$select_query->bind_param("ii", $user_id, $movie_id);
-$select_query->execute();
-
-$select_result = $select_query->get_result();
-if ($response->num_rows != 0) {
-    echo json_encode([
-        "status" => "Success"
-    ])
-}else{
-    echo json_encode([
-        "status" => "Error"
-    ])
-}
-
 $query = $connection->prepare("INSERT INTO user_activities (user_id, movie_id, nb_of_clicks, time_spent) VALUES (?, ?, ?, ?) ON DUPLICATE KEY UPDATE nb_of_clicks = ?, time_spent = ?");
 $query->bind_param("iiidid", $user_id, $movie_id, $nb_of_clicks, $time_spent, $nb_of_clicks, $time_spent);
 $query->execute();
@@ -39,4 +24,4 @@ if($result != 0){
     echo json_encode([
         "status" => "Failed to add activity"
     ]);
-}*/
+}
