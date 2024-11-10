995ddbbe2bae474f5dd9d2714fc357fb6f9c5170
LyneSultan <lynesultane@gmail.com>
2024-11-10 23:58:41 +0200

    Add - html files

--- server-side/getAllMovies.php
+++ server-side/getAllMovies.php
@@ -2,26 +2,48 @@
 
 include "connection.php";
 
-$query=$connection->prepare("SELECT movie_id,title,img from movies");
-$query->execute();
+$movie_id=$_POST['movie_id']??NULL;
+if($movie_id==NULL)
+{
+  $query=$connection->prepare("SELECT movie_id,title,img from movies");
+  $query->execute();
 
-$result=$query->get_result();
+  $result=$query->get_result();
 
-if($result->num_rows>0){
+  if($result->num_rows>0){
 
-  $movies=[];
-  while($movie=$result->fetch_assoc())
-  {
-    $movies[]=$movie;
+    $movies=[];
+    while($movie=$result->fetch_assoc())
+    {
+      $movies[]=$movie;
 
+    }
+    echo json_encode($movies);
+  } else {
+
+    $response = [
+      "message" => "Empty result"
+    ];
+
+    echo json_encode($response);
   }
-  echo json_encode($movies);
-} else {
-
-  $response = [
-    "message" => "Empty result"
-  ];
-
-  echo json_encode($response);
 }
+else {
+  $query = $connection->prepare("SELECT * FROM movies WHERE movie_id = ?");
+  $query->bind_param("i",$movie_id);
+
+  $query->execute();
+
+  $result = $query->get_result();
+
+  if($result->num_rows > 0) {
+    $user = $result->fetch_assoc();
+
+    echo json_encode($user);
+  } else {
+    echo json_encode([
+      "message" => "Not Found"
+    ]);
+  }
+} 
 ?>
