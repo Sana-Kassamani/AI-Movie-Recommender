82ab06a8081b17e904c9279b9f9abe4bb15e2e5e
Sana <Lenovo@DESKTOP-I4OODFD>
2024-11-13 02:07:42 +0200

    Update - getMovieActivity api to give useful insights

--- server-side/getMovieActivity.php
+++ server-side/getMovieActivity.php
@@ -2,22 +2,59 @@
 
 include "connection.php";
 
+$row = [];
+$avg_clicks=0;
+$avg_time = 0;
 
-$query=$connection->prepare("SELECT m.*,SUM(nb_of_clicks) as sum_of_clicks ,SUM(time_spent) as total_time FROM movies as m LEFT JOIN user_activities as ua ON m.movie_id = ua.movie_id GROUP BY m.movie_id;" );
+$get_avg_query=$connection->prepare("SELECT AVG(nb_of_clicks) as avg_clicks ,AVG(time_spent) as avg_time FROM movies as m LEFT JOIN user_activities as ua ON m.movie_id = ua.movie_id;" );
+$get_avg_query->execute();
+$get_avg_result = $get_avg_query->get_result();
+if($get_avg_result){
+    $row = $get_avg_result->fetch_assoc();
+    $avg_clicks = $row["avg_clicks"];
+    $avg_time= $row["avg_time"];
+}
+else{
+    echo json_encode([
+        "message"=>"No avg of clicks and time on movies"
+    ]);
+}
+
+function get_average($movie, $avg_clicks,$avg_time)
+{
+    if($avg_clicks){
+        $movie["nb_of_clicks"]= round(($movie["nb_of_clicks"]/$avg_clicks),2);
+    }
+    if($avg_time){
+        $movie["time_spent"]= round(($movie["time_spent"]/$avg_time),2) ;
+    }
+
+    return $movie; 
+
+}
+
+
+$query=$connection->prepare("SELECT m.title,m.avg_rating,m.image_src,ua.nb_of_clicks,ua.time_spent FROM movies as m LEFT JOIN user_activities as ua ON m.movie_id = ua.movie_id;" );
 $query->execute();
 $result = $query->get_result();
-if($result){
-    $array=[];
-    while($movie = $result->fetch_assoc())
+if($result->num_rows>0){
+    $movies = [];
+    while($movie =$result->fetch_assoc())
     {
-        $array[]= $movie;
+        $movies[]=$movie;
     }
+
+    $movies_with_avg = array_map(function($movie) use ($avg_clicks, $avg_time) {
+        return get_average($movie, $avg_clicks, $avg_time);
+    }, $movies);
     echo json_encode([
-        "movies"=>$array,
-    ]);
+         "movies"=> $movies_with_avg
+       ,]
+    );
 }
 else{
     echo json_encode([
         "message"=>"No movies"
     ]);
+
 }
