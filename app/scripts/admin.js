dde8c3ac36ddd9789b44ff2aa9f5ae9fea733ec9
Sana <Lenovo@DESKTOP-I4OODFD>
2024-11-13 03:12:51 +0200

    Add - show all movies on admin panel <> JS

--- app/scripts/admin.js
+++ app/scripts/admin.js
@@ -1,5 +1,5 @@
 const users = document.getElementById("all-users");
-const movies = document.getElementById("all-movies");
+const moviesSection = document.getElementById("all-movies");
 
 const toggleBan = (user, isBanned) => {
   let data = new FormData();
@@ -64,6 +64,61 @@ function displayUser(user) {
   return div_user;
 }
 
+const showAllMovies = () => {
+  axios(
+    "http://localhost/AI-Movie-Recommender/server-side/getMovieActivity.php",
+    {
+      method: "GET",
+    }
+  )
+    .then((response) => {
+      console.log(response.data.movies);
+      response.data.movies.forEach((movie) => {
+        const div_movie = displayMovie(movie);
+        moviesSection.appendChild(div_movie);
+      });
+    })
+    .catch((error) => {
+      console.log("Error showing all movies", error);
+    });
+};
+
+function displayMovie(movie) {
+  const div_movie = document.createElement("div");
+  div_movie.setAttribute("class", "flex movie");
+  div_movie.innerHTML = "";
+  div_movie.innerHTML = `
+    <div>
+      <img class="img-div" src="${movie.image_src}" alt="" />
+    </div>
+    <div class="flex column">
+      <p>Movie Title</p>
+      <p>${movie.title}</p>
+      <div class="stars">
+        
+      </div>
+    </div>
+    <div class="flex column">
+      <p>Ratio of Total Clicks</p>
+      <p>${movie.nb_of_clicks}</p>
+    </div>
+    <div class="flex column">
+      <p>Ratio of Total Time</p>
+      <p>${movie.time_spent}</p>
+    </div>
+  `;
+
+  const stars = div_movie.querySelector(".stars");
+  stars.innerHTML = "";
+  for (let i = 1; i <= 5; i++) {
+    stars.innerHTML += `
+    <i class="fa-solid fa-star" ${"active" ? i <= movie.avg_rating : ""}></i>
+    `;
+  }
+  return div_movie;
+}
+
 document.addEventListener("DOMContentLoaded", async () => {
   await showAllUsers();
+  await showAllMovies();
 });
